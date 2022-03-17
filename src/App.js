de3ea90522789b40adb8f874006873d07581cfab
hadooken <kennyqng@gmail.com>
2022-03-16 22:26:13 -0700

    add baseName to Router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router basename={"/score"}>
       <Routes>
         <Route path="/" element={<Add />}></Route>
       </Routes>
